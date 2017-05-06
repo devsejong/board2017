efaf1faed11abf970811074f414365d6b0a6dea2
sjpark <devsejong@gmail.com>
2017-05-06 18:51:10 +0900

    refactor : 초기화데이터 생성장소 교체

--- src/main/java/net/chandol/study/board/local/LocalTestDataGenerator.java
+++ src/main/java/net/chandol/study/board/local/LocalTestDataGenerator.java
@@ -1,26 +1,33 @@
-package net.chandol.study.board.article.service.util;
+
+package net.chandol.study.board.local;
 
 import net.chandol.study.board.article.dto.ArticleCreateRequest;
 import net.chandol.study.board.article.service.ArticleService;
+import net.chandol.study.board.user.User;
+import net.chandol.study.board.user.UserCreateRequest;
+import net.chandol.study.board.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.stream.IntStream;
 
-@Service
 @Profile("local")
-public class ArticleDummyAdderService {
-    @Autowired ArticleService service;
+@Configuration
+public class LocalTestDataGenerator {
+    @Autowired UserService userService;
+    @Autowired ArticleService articleService;
 
     @PostConstruct
     @Transactional
     public void addDummyArticles() {
+        User user = userService.createUser(new UserCreateRequest("admin", "email", "admin"));
+
         IntStream.rangeClosed(1, 128).forEach(i -> {
             ArticleCreateRequest request = new ArticleCreateRequest("제목 " + i, "작가 " + i, "본문 " + i);
-            service.createArticle(request);
+            articleService.createArticle(request);
         });
     }
 }
