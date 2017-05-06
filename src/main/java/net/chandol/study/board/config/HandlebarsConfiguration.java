f9f769c50eae1867d30de555ccf56058b5bd704a
sjpark <devsejong@gmail.com>
2017-05-06 17:57:03 +0900

    이름변경

--- src/main/java/net/chandol/study/board/config/HandlebarsConfiguration.java
+++ src/main/java/net/chandol/study/board/config/HandlebarsConfiguration.java
@@ -14,7 +14,7 @@ import java.time.OffsetDateTime;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 @Configuration
-public class Java8DateHelper {
+public class HandlebarsConfiguration {
 
     @Autowired HandlebarsViewResolver handlebarsViewResolver;
 
